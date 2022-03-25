4ea5cf656b394cab8b3c721d0de943d93e637006
AimanRohaizi190030 <80297218+Aiman3141592@users.noreply.github.com>
2022-03-25 17:26:53 +0800

    Add files via upload

--- quser_define.py
+++ quser_define.py
@@ -1,11 +1,19 @@
 ###### user define #######
 
-#state what is the desired galaxy cluster name 'A4038' or 'A1758S' or 'A1758N'
-cluster_name='A4038'
+#state what is the desired galaxy cluster name 'A4038' or 'A1758'
+cluster_name='A1758'
 
 #state the desired annhilation branch: 'bbx' or 'mummup' or 'all'
 
-an_b='mummup'
+an_b='bbx'
+
+
+'''
+to get the outcomes:
+1. open terminal in the output file
+2. type:     python2 kkrunthiswithoutloss.py
+3. pree Enter
+'''
 
 
 
